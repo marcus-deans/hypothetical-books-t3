d27cdcbb407f1fc96e7731a974452e509067d2db
JakeVigeantProfessional <jmv43duke.edu>
2023-01-29 21:55:58 -0500

    added type for book component props

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -1,6 +1,15 @@
 type BookProps = {
-    title?:String,
+    title:String,
+    authors:string[],
+    ISBN13:number,
+    ISBN10?:number,
+    publisher:String,
+    publicationYear:number,
     pageCount?:number,
+    //Handle Dimensions
+    retailPrice:number,
+    genre:String
+
 }
 
 function Book(props:BookProps) {
