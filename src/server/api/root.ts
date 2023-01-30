f5445a4f6da328423ee7e9bdbb865d0041e1d81b
Marcus Deans <57301442+marcus-deans@users.noreply.github.com>
2023-01-30 13:40:29 -0500

    Added routers to main file

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,13 @@
 import { createTRPCRouter } from "./trpc";
 import { exampleRouter } from "./routers/example";
+import { authorsRouter } from "./routers/authors";
+import { booksRouter } from "./routers/books";
+import { genresRouter } from "./routers/genres";
+import { purchaseLinesRouter } from "./routers/purchaseLines";
+import { purchaseOrdersRouter } from "./routers/purchaseOrders";
+import { salesLinesRouter } from "./routers/salesLines";
+import { salesReconciliationsRouter } from "./routers/salesReconciliations";
+import { vendorsRouter } from "./routers/vendors";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +16,14 @@ import { exampleRouter } from "./routers/example";
  */
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
+  authors: authorsRouter,
+  books: booksRouter,
+  genres: genresRouter,
+  purchaseLines: purchaseLinesRouter,
+  purchaseOrders: purchaseOrdersRouter,
+  salesLines: salesLinesRouter,
+  salesReconciliations: salesReconciliationsRouter,
+  vendors: vendorsRouter,
 });
 
 // export type definition of API
