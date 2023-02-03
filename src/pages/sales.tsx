589e54bad5c1aec17600693e4ef1acba15ecc196
Marcus Deans <57301442+marcus-deans@users.noreply.github.com>
2023-02-03 01:55:19 -0500

    Created initial sales line, sales reconciliation, and fixed prettier config for tailwind

--- src/pages/sales.tsx
+++ src/pages/sales.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import Head from 'next/head'
+import React from "react";
+import Head from "next/head";
+import SalesLine from "../components/SalesLine";
+
 function sales() {
   return (
     <>
-    <Head>
-      <title>Sales</title>
-    </Head>
-    <div>sales</div>
+      <Head>
+        <title>Sales</title>
+      </Head>
+      <div>sales</div>
+      <SalesLine quantity={40} unitWholesalePrice={3.99} />
     </>
-  )
+  );
 }
 
-export default sales
+export default sales;
