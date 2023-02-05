292e2bac6c6fe9e44d991329a98bef47da89e615
JakeVigeantProfessional <jmv43duke.edu>
2023-02-05 16:28:33 -0500

    test

--- src/pages/books.tsx
+++ src/pages/books.tsx
@@ -13,6 +13,7 @@ function books() {
     </Head>
     <Searchbar updateSearch = {stateUpdateWrapper}></Searchbar>
     <div>{searchQuery}</div>
+    <Book title="To Kill a Mockingbird" authors={["Harper Lee"]} publisher="penguin" ISBN13={12345678910111213} publicationYear={2010} retailPrice={10} genre={"Fiction"}></Book>
     </>
   )
 }
