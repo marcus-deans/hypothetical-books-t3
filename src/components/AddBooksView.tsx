a080dae264839ec93ef8820ffec75e73bc48f755
JakeVigeantProfessional <jmv43duke.edu>
2023-02-05 21:05:41 -0500

    Input Bar to enter ISBN list

--- src/components/AddBooksView.tsx
+++ src/components/AddBooksView.tsx
@@ -1,5 +1,14 @@
+import { InputBar } from "./InputBar"
+import { useState } from "react"
 export const AddBooksView = () => {
-  return (
-    <div>AddBooksView</div>
+const[SearchComplete, setSearchComplete] = useState(false);
+
+const searchCompleteWrapper = () => {
+    setSearchComplete(true);
+  };  return (
+    <>
+    <InputBar setSearchComplete = {searchCompleteWrapper}/>
+    {SearchComplete ? ( <div>ISBN Lists Go Here</div>) :( <div className="flex h-screen w-screen justify-center items-center text-4xl text-white">Please Search for ISBNs to View Book Information</div>)}
+    </>
   )
 }
