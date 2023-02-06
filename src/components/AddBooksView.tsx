ff660f0c1fa1f0d750a320efb4b7bf207eaa0507
JakeVigeantProfessional <jmv43duke.edu>
2023-02-06 11:48:26 -0500

    Allow add page access to ISBN search terms

--- src/components/AddBooksView.tsx
+++ src/components/AddBooksView.tsx
@@ -1,14 +1,22 @@
 import { InputBar } from "./InputBar"
 import { useState } from "react"
+import { TableView } from "./TableView";
 export const AddBooksView = () => {
 const[SearchComplete, setSearchComplete] = useState(false);
+const[ISBNSearches, setISBNSearches] = useState<string[]>([])
+
+const ISBNSearchesWrapper = (list: string[]) => {setISBNSearches(ISBNSearches => [...ISBNSearches, ...list])}
 
 const searchCompleteWrapper = () => {
     setSearchComplete(true);
   };  return (
     <>
-    <InputBar setSearchComplete = {searchCompleteWrapper}/>
-    {SearchComplete ? ( <div>ISBN Lists Go Here</div>) :( <div className="flex h-screen w-screen justify-center items-center text-4xl text-white">Please Search for ISBNs to View Book Information</div>)}
+    <InputBar setSearchComplete = {searchCompleteWrapper} setISBNSearchesUpper = {ISBNSearchesWrapper}/>
+    {SearchComplete ? (         
+    <div>
+            <TableView labels={["Title", "Author", "ISBN", "Retail Price", "Genre","Inventory Count"]}></TableView>
+        </div>
+) :( <div className="flex h-screen w-screen justify-center items-center text-4xl text-white">Please Search for ISBNs to View Book Information</div>)}
     </>
   )
 }
--- src/components/InputBar.tsx
+++ src/components/InputBar.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react"
+import { TableView } from "./TableView";
 interface InputBarProps{
     setSearchComplete: () => void;
+    setISBNSearchesUpper: (list: string[]) => void;
 }
 
 
@@ -12,14 +14,14 @@ const handleISBNSubmit = () =>{props.setSearchComplete()
     for (let i = 0; i < searchSplit.length; i++) {
         setISBNSearches(ISBNSearches => [...(ISBNSearches ?? []), searchSplit[i] ?? ""]);
     }
+    props.setISBNSearchesUpper(ISBNSearches);
     setSearchValue("");
 }
 const handleSearchTyping = (e:any) => {
     setSearchValue(e.target.value)
 
 }
-//setMyArray(oldArray => [...oldArray, newElement]);
-  return (
+return (
     <>
         <div className="relative w-full">
             <input type="search" onChange = {handleSearchTyping}id="search-dropdown" className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50  border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" value = {searchValue} placeholder = "Enter ISBNs To Find..."required />
