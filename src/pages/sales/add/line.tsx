edd10dcbdd8a9a8d1619bc855e75afd3d8cbc158
Marcus Deans <57301442+marcus-deans@users.noreply.github.com>
2023-02-08 12:09:02 -0500

    Initial versions of sales line & reconciliation adding

--- src/pages/sales/add/line.tsx
+++ src/pages/sales/add/line.tsx
@@ -1,7 +1,7 @@
 import { ReactSearchAutocomplete } from "react-search-autocomplete";
 import Head from "next/head";
-import React from "react";
-import type { SalesReconciliation } from ".prisma/client";
+import React, { useState } from "react";
+import type { Book, SalesReconciliation } from ".prisma/client";
 import type {
   GetServerSidePropsContext,
   InferGetServerSidePropsType,
@@ -11,78 +11,163 @@ import { appRouter } from "../../../server/api/root";
 import { createInnerTRPCContext } from "../../../server/api/trpc";
 import superjson from "superjson";
 import { api } from "../../../utils/api";
-import { getBindingIdentifiers } from "@babel/types";
+import type { Dayjs } from "dayjs";
+import { router } from "next/client";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+import TextField from "@mui/material/TextField";
+import { z } from "zod";
 
-export default function AddSaleLine(
+export default function AddSalesLine(
   props: InferGetServerSidePropsType<typeof getServerSideProps>
 ) {
   const salesReconciliationsQuery = api.salesReconciliations.getAll.useQuery({
     cursor: null,
-    limit: 50,
+    limit: 100,
   });
+  const booksQuery = api.books.getAll.useQuery({ cursor: null, limit: 100 });
 
   const salesReconciliations = salesReconciliationsQuery?.data?.items ?? [];
-  const handleOnSearch = (string, results) => {
-    // onSearch will have as the first callback parameter
-    // the string searched and for the second the results.
-    console.log(string, results);
+  const books = booksQuery?.data?.items ?? [];
+  const [dateValue, setDateValue] = useState<Dayjs | null>();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const addMutation = api.salesLines.add.useMutation();
+
+  const [state, setState] = useState({
+    bookId: "",
+    quantity: 1,
+    unitWholesalePrice: 0,
+    salesReconciliationId: "",
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setState({ ...state, [event.target.name]: event.target.value });
+  };
+  const handleSubmit = () => {
+    setIsSubmitting(true);
+    try {
+      if (!dateValue) {
+        throw new Error("Date is required");
+      }
+      if (!state.bookId || !state.salesReconciliationId) {
+        throw new Error("Book and Sales Reconciliation are required");
+      }
+      const addResult = addMutation.mutate({
+        bookId: state.bookId,
+        quantity: state.quantity,
+        unitWholesalePrice: state.unitWholesalePrice,
+        salesReconciliationId: state.salesReconciliationId,
+      });
+      setTimeout(() => {
+        void router.push(
+          `/sales/${encodeURIComponent(state.salesReconciliationId)}`
+        );
+      }, 500);
+    } catch (error) {
+      console.log(error);
+      setIsSubmitting(false);
+    }
   };
 
-  const handleOnHover = (result) => {
-    // the item hovered
-    console.log(result);
-  };
-
-  const handleOnSelect = (item) => {
+  const handleOnSelectSalesReconciliation = (item: SalesReconciliation) => {
     // the item selected
-    console.log(item);
+    setState({ ...state, salesReconciliationId: item.id });
   };
 
-  const handleOnFocus = () => {
-    console.log("Focused");
+  const handleOnSelectBook = (item: Book) => {
+    // the item selected
+    setState({ ...state, bookId: item.id });
   };
 
-  const formatResult = (item: SalesReconciliation) => {
+  const formatResultSalesReconciliation = (item: SalesReconciliation) => {
     return (
       <div>
         <span style={{ display: "block", textAlign: "left" }}>
           id: {item.id}
         </span>
         <span style={{ display: "block", textAlign: "left" }}>
-          name: {item.date.toDateString()}
+          date: {item.date.toDateString()}
+        </span>
+      </div>
+    );
+  };
+
+  const formatResultBook = (item: Book) => {
+    return (
+      <div>
+        <span style={{ display: "block", textAlign: "left" }}>
+          title: {item.title}
+        </span>
+        <span style={{ display: "block", textAlign: "left" }}>
+          isbn-13: {item.isbn_13}
         </span>
       </div>
     );
   };
 
   return (
-    <div>
-      <Head>
-        <title>Add</title>
-      </Head>
-      <div className="space-y-5">
-        <div className="grid grid-cols-5 gap-3">
-          <div className="rounded-lg bg-white py-1 text-center">
-            Sales Reconciliation
+    <div className="flex w-full items-center ">
+      <form className="mb-4 w-3/4 rounded bg-white px-8 pt-6 pb-8 shadow-md">
+        <div className="mb-4 items-center space-y-5">
+          <div className="mb-2 block text-lg font-bold text-gray-700">
+            Create Sales Line
           </div>
-          <div className="col-span-4">
-            <ReactSearchAutocomplete<SalesReconciliation>
-              items={salesReconciliations}
-              fuseOptions={{ keys: ["id", "date"] }}
-              placeholder="Search for a sales reconciliation"
-              onSearch={handleOnSearch}
-              onHover={handleOnHover}
-              onSelect={handleOnSelect}
-              onFocus={handleOnFocus}
-              autoFocus
-              formatResult={formatResult}
+          <div className="relative space-y-3">
+            <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3"></div>
+            <div className="col-span-4">
+              <ReactSearchAutocomplete<SalesReconciliation>
+                items={salesReconciliations}
+                fuseOptions={{ keys: ["id", "date"] }}
+                placeholder="Search for a sales reconciliation"
+                onSelect={handleOnSelectSalesReconciliation}
+                autoFocus
+                formatResult={formatResultSalesReconciliation}
+              />
+            </div>
+            <div className="col-span-4">
+              <ReactSearchAutocomplete<Book>
+                items={books}
+                fuseOptions={{ keys: ["title", "isbn"] }}
+                placeholder="Search for a book"
+                onSelect={handleOnSelectBook}
+                autoFocus
+                formatResult={formatResultBook}
+              />
+            </div>
+            <input
+              className="focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
+              id="quantity"
+              name="quantity"
+              type="text"
+              placeholder="Quantity"
+              min="1"
+              onChange={handleChange}
+              required
+            />
+            <input
+              className="focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
+              id="UnitWholesalePrice"
+              name="UnitWholesalePrice"
+              type="text"
+              placeholder="Unit Wholesale Price"
+              min="0"
+              onChange={handleChange}
+              required
             />
           </div>
         </div>
-        <div className="rounded-xl bg-white py-3 text-center">
-          Save Sales Line
+        <div className="flex items-center justify-between">
+          <button
+            className="focus:shadow-outline rounded bg-blue-500 py-2 px-4 align-middle font-bold text-white hover:bg-blue-700 focus:outline-none"
+            type="button"
+            onClick={handleSubmit}
+          >
+            {isSubmitting ? "Submitting..." : "Submit"}
+          </button>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
@@ -99,7 +184,8 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
    * Prefetching the `post.byId` query here.
    * `prefetch` does not return the result and never throws - if you need that behavior, use `fetch` instead.
    */
-  await ssg.salesReconciliations.getAll.prefetch({ cursor: null, limit: 50 });
+  await ssg.salesReconciliations.getAll.prefetch({ cursor: null, limit: 100 });
+  await ssg.books.getAll.prefetch({ cursor: null, limit: 100 });
   // Make sure to return { props: { trpcState: ssg.dehydrate() } }
   return {
     props: {
