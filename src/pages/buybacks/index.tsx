8b0f7c0fd4efdee6c4b8c2ed35bbc22a035642d2
JakeVigeantProfessional <jmv43duke.edu>
2023-02-23 13:37:35 -0500

    Added buybacks page

---
+++ src/pages/buybacks/index.tsx
@@ -0,0 +1,14 @@
+import  Head  from "next/head"
+
+const index = () => {
+  return (
+    <>
+     <Head>
+    <title>BuyBacks</title>
+    </Head>
+    <div>BuyBacks</div>
+    </>
+  )
+}
+
+export default index
