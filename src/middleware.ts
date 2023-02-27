b5085933daa2f19ab10c1c395e0fa81219399d4b
Nick Ward <nw121@duke.edu>
2023-02-27 13:22:47 -0500

    protected buybacks page in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { withAuth } from "next-auth/middleware";
 export default withAuth(
   // `withAuth` augments your `Request` with the user's token.
   function middleware(req) {
-    console.log("In middlware");
+    //console.log("In middleware");
   },
   {
     callbacks: {
@@ -24,6 +24,7 @@ export const config = {
     "/books/:path*",
     "/sales/:path*",
     "/purchases/:path*",
+    "/buybacks/:path*",
     "/vendors/:path*",
     "/authors/:path*",
     "/genres/:path*",
