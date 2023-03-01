d27f0418af52738d7bbe72df0cd8ba5e0a4fcde2
Marcus Deans <marcus.deans@duke.edu>
2023-03-01 14:48:30 -0800

    Fix ISBN handling

--- src/pages/books/add.tsx
+++ src/pages/books/add.tsx
@@ -131,10 +131,10 @@ export default function AddBook() {
           isbn_10 = book.industryIdentifiers[1].identifier;
         }
       }
-      if (book?.industryIdentifiers[0]?.type === "ISBN_13") {
-        isbn_13 = book.industryIdentifiers[0].identifier;
-        if (book?.industryIdentifiers[1]?.type === "ISBN_10") {
-          isbn_10 = book.industryIdentifiers[1].identifier;
+      if (book?.industryIdentifiers[1]?.type === "ISBN_13") {
+        isbn_13 = book.industryIdentifiers[1].identifier;
+        if (book?.industryIdentifiers[0]?.type === "ISBN_10") {
+          isbn_10 = book.industryIdentifiers[0].identifier;
         }
       }
 
@@ -237,10 +237,10 @@ export default function AddBook() {
         isbn_10 = retrievedBook.industryIdentifiers[1].identifier;
       }
     }
-    if (retrievedBook?.industryIdentifiers[0]?.type === "ISBN_13") {
-      isbn_13 = retrievedBook.industryIdentifiers[0].identifier;
-      if (retrievedBook?.industryIdentifiers[1]?.type === "ISBN_10") {
-        isbn_10 = retrievedBook.industryIdentifiers[1].identifier;
+    if (retrievedBook?.industryIdentifiers[1]?.type === "ISBN_13") {
+      isbn_13 = retrievedBook.industryIdentifiers[1].identifier;
+      if (retrievedBook?.industryIdentifiers[0]?.type === "ISBN_10") {
+        isbn_10 = retrievedBook.industryIdentifiers[0].identifier;
       }
     }
 
