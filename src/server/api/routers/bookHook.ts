9cc18020e6f63d08d094f80a5699995a1c913229
Marcus Deans <marcus.deans@duke.edu>
2023-03-14 19:04:35 -0600

    Add sales logic implementation

--- src/server/api/routers/bookHook.ts
+++ src/server/api/routers/bookHook.ts
@@ -2,6 +2,7 @@ import { createTRPCRouter, publicProcedure } from "../trpc";
 import { z } from "zod";
 import { TRPCError } from "@trpc/server";
 import { XMLParser } from "fast-xml-parser";
+import { prisma } from "../../db";
 
 export const bookHookRouter = createTRPCRouter({
   upload: publicProcedure
@@ -37,9 +38,9 @@ export const bookHookRouter = createTRPCRouter({
         })
       )
     )
-    .mutation(({ input }) => {
+    .mutation(async ({ input }) => {
       const options = {
-        ignoreAttributes: true,
+        ignoreAttributes: false,
       };
       const xml = Object.values(input).join("");
 
@@ -55,18 +56,24 @@ export const bookHookRouter = createTRPCRouter({
       });
       const BookHookDataSchema = z.object({
         item: z.array(z.any()),
+        "@_date": z.string(),
       });
       const BookHookInputSchema = z.object({
-        "?xml": z.string(),
+        "?xml": z.object({
+          "@_version": z.string(),
+          "@_encoding": z.string(),
+        }),
         sale: BookHookDataSchema,
       });
       type BookHookInput = z.infer<typeof BookHookInputSchema>;
       type BookHookData = z.infer<typeof BookHookDataSchema>;
       type BookHookItem = z.infer<typeof BookHookItemSchema>;
+      let salesDate = "";
       const processData = (parsedData: BookHookData) => {
         console.log(parsedData);
         if (BookHookDataSchema.safeParse(parsedData).success) {
           console.log("Data is valid");
+          salesDate = parsedData["@_date"];
           const salesData: BookHookItem[] = [];
           parsedData.item.forEach((item) => {
             if (BookHookItemSchema.safeParse(item).success) {
@@ -102,6 +109,73 @@ export const bookHookRouter = createTRPCRouter({
         throw new TRPCError({
           code: "PARSE_ERROR",
           message: "No ISBNs could be found",
+        });
+      }
+
+      const salesDateFormatted = new Date(salesDate);
+      const salesReconciliation = await prisma.salesReconciliation.create({
+        data: {
+          date: salesDateFormatted,
+        },
+      });
+
+      for (const sale of processedData) {
+        const currentBook = await prisma.book.findFirst({
+          where: {
+            OR: [
+              {
+                isbn_10: sale.isbn,
+              },
+              {
+                isbn_13: sale.isbn,
+              },
+            ],
+          },
+        });
+        if (!currentBook) {
+          console.log(`Book with ISBN ${sale.isbn} not found`);
+          continue;
+        }
+
+        const bookInventoryCount = await prisma.book.findUnique({
+          where: { id: currentBook.id },
+          select: {
+            inventoryCount: true,
+          },
+        });
+
+        if ((bookInventoryCount?.inventoryCount ?? 0) < sale.qty) {
+          throw new TRPCError({
+            code: "PRECONDITION_FAILED",
+            message: `Not enough inventory for sale of ${sale.qty}} of book with ISBN ${sale.isbn}`,
+          });
+        }
+
+        const salesLine = await prisma.salesLine.create({
+          data: {
+            book: {
+              connect: {
+                id: currentBook.id,
+              },
+            },
+            quantity: sale.qty,
+            unitWholesalePrice: sale.price,
+            salesReconciliation: {
+              connect: {
+                id: salesReconciliation.id,
+              },
+            },
+            display: true,
+          },
+        });
+
+        await prisma.book.update({
+          where: { id: currentBook.id },
+          data: {
+            inventoryCount: {
+              decrement: sale.qty,
+            },
+          },
         });
       }
       return processedData;
