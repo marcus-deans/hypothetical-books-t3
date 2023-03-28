88a4f37cc4e1623f989ad09e6d87092d8bd96074
Andrew Demma <andrew.demma@duke.edu>
2023-03-28 07:33:42 -0400

    Fix vendor 0 buyback

--- src/pages/vendors/add.tsx
+++ src/pages/vendors/add.tsx
@@ -20,10 +20,10 @@ export default function AddVendor() {
       const finalBuybackRate = Number(buybackRate);
       if (
         isNaN(finalBuybackRate) ||
-        finalBuybackRate <= 0 ||
+        finalBuybackRate < 0 ||
         finalBuybackRate >= 100
       ) {
-        alert("Buyback rate must be a number between 0 and 100");
+        alert("Buyback rate must be a number between 0 and 100, or 0 to represent no buybacks for this vendor");
         return;
       }
       addMutation.mutate({ name: vendorName, buybackRate: finalBuybackRate });
